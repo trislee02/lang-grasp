e4c789327e7c69c7ff3ab3b540c444e217b2a710
Le Quang Tri <quangtri2002lk@gmail.com>
2024-04-28 07:58:58 +0700

    test with one loss

--- train_network_lgrasp.py
+++ train_network_lgrasp.py
@@ -327,15 +327,6 @@ def run():
 
     logging.info('Trainable Parameters: {}'.format(trainable_params_count))
 
-    # # Print model architecture.
-    # input_channels = 1 * args.use_depth + 3 * args.use_rgb
-    # summary(net, (input_channels, args.input_size, args.input_size))
-    # f = open(os.path.join(save_folder, 'arch.txt'), 'w')
-    # sys.stdout = f
-    # summary(net, (input_channels, args.input_size, args.input_size))
-    # sys.stdout = sys.__stdout__
-    # f.close()
-
     best_iou = 0.0
     for epoch in range(args.epochs):
         logging.info('Beginning Epoch {:02d}'.format(epoch))
