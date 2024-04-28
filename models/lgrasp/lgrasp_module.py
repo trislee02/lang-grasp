14bf884114e52a8a5bc2791b95fe4998e0336743
Le Quang Tri <quangtri2002lk@gmail.com>
2024-04-28 17:13:07 +0700

    add training_epoch_end

--- models/lgrasp/lgrasp_module.py
+++ models/lgrasp/lgrasp_module.py
@@ -1,16 +1,21 @@
 import torch
 import torch.cuda.amp as amp
 import pytorch_lightning as pl
-
+from inference.post_process import post_process_output
+from utils.dataset_processing import evaluation
 
 class LGraspModule(pl.LightningModule):
-    def __init__(self, model, loss_fn, optimizer, scheduler, max_epochs=100, base_lr=4e-3):
+    def __init__(self, 
+                 model, 
+                 loss_fn, 
+                 dataset,
+                 max_epochs=100, 
+                 base_lr=4e-3):
         super().__init__()
         self.model = model
         self.loss_fn = loss_fn
-        self.optimizer = optimizer
-        self.scheduler = scheduler
         self.base_lr = base_lr
+        self.dataset = dataset
         
         self.epochs = max_epochs
         self.enabled = False #True mixed precision will make things complicated and leading to NAN error
@@ -20,24 +25,49 @@ class LGraspModule(pl.LightningModule):
         return self.model(x)
 
     def training_step(self, batch, batch_idx):
-        print("Batch: ", batch_idx)
         x, y, _, _, _ = batch
         xc = (x[0], x[1]) # x[0] is a Tensor [batch_size, c, h, w], x[1] is a Tuple of `batch_size`` prompts
         yc = [yy for yy in y]
         with amp.autocast(enabled=self.enabled):
             lossd = self.loss_fn(xc, yc)
             loss = lossd['loss']
-        print("Loss: ", loss.item())
         self.log("train_loss", loss)
         return loss
 
+    def training_epoch_end(self, outs):
+        # TODO: Add metric calculation
+        print("Epoch end", outs)
+
     def validation_step(self, batch, batch_idx):
-        x, y, _, _, _ = batch
+        x, y, didx, rot, zoom_factor = batch
         xc = (x[0], x[1]) # x[0] is a Tensor [batch_size, c, h, w], x[1] is a Tuple of `batch_size`` prompts
         yc = [yy for yy in y]
         lossd = self.loss_fn(xc, yc)
         loss = lossd['loss']
-        print("Val loss: ", loss.item())
+
+        results = {
+                    'correct': 0,
+                    'failed': 0,
+                    'loss': 0,
+                    'losses': {}
+                }
+
+        q_out, ang_out, w_out = post_process_output(lossd['pred']['pos'], lossd['pred']['cos'],
+                                                        lossd['pred']['sin'], lossd['pred']['width'])
+
+        s = evaluation.calculate_iou_match(q_out,
+                                            ang_out,
+                                            self.dataset.get_gtbb(didx, rot, zoom_factor),
+                                            no_grasps=1,
+                                            grasp_width=w_out,
+                                            threshold=0.25
+                                            )
+
+        if s:
+            results['correct'] += 1
+        else:
+            results['failed'] += 1
+
         self.log("val_loss", loss)
         return loss
 
--- train_network_lgrasp_pl.py
+++ train_network_lgrasp_pl.py
@@ -25,8 +25,6 @@ from utils import count_parameters, parameters_grad
 from models.lgrasp import make_model, make_trainer
 from models.lgrasp import LGraspModule
 
-import pytorch_lightning as pl
-
 def parse_args():
     parser = argparse.ArgumentParser(description='Train network')
 
@@ -101,131 +99,6 @@ def parse_args():
     args = parser.parse_args()
     return args
 
-
-def validate(net, device, val_data, iou_threshold):
-    """
-    Run validation.
-    :param net: Network
-    :param device: Torch device
-    :param val_data: Validation Dataset
-    :param iou_threshold: IoU threshold
-    :return: Successes, Failures and Losses
-    """
-    net.eval()
-
-    results = {
-        'correct': 0,
-        'failed': 0,
-        'loss': 0,
-        'losses': {
-
-        }
-    }
-
-    ld = len(val_data)
-
-    with torch.no_grad():
-        for x, y, didx, rot, zoom_factor in val_data:
-            xc = x.to(device)
-            yc = [yy.to(device) for yy in y]
-            lossd = net.compute_loss(xc, yc)
-
-            loss = lossd['loss']
-
-            results['loss'] += loss.item() / ld
-            for ln, l in lossd['losses'].items():
-                if ln not in results['losses']:
-                    results['losses'][ln] = 0
-                results['losses'][ln] += l.item() / ld
-
-            q_out, ang_out, w_out = post_process_output(lossd['pred']['pos'], lossd['pred']['cos'],
-                                                        lossd['pred']['sin'], lossd['pred']['width'])
-
-            s = evaluation.calculate_iou_match(q_out,
-                                               ang_out,
-                                               val_data.dataset.get_gtbb(didx, rot, zoom_factor),
-                                               no_grasps=1,
-                                               grasp_width=w_out,
-                                               threshold=iou_threshold
-                                               )
-
-            if s:
-                results['correct'] += 1
-            else:
-                results['failed'] += 1
-
-    return results
-
-
-def train(epoch, net, device, train_data, optimizer, batches_per_epoch, vis=False):
-    """
-    Run one training epoch
-    :param epoch: Current epoch
-    :param net: Network
-    :param device: Torch device
-    :param train_data: Training Dataset
-    :param optimizer: Optimizer
-    :param batches_per_epoch:  Data batches to train on
-    :param vis:  Visualise training progress
-    :return:  Average Losses for Epoch
-    """
-    results = {
-        'loss': 0,
-        'losses': {
-        }
-    }
-
-    net.train()
-
-    batch_idx = 0
-    # Use batches per epoch to make training on different sized datasets (cornell/jacquard) more equivalent.
-    while batch_idx <= batches_per_epoch:
-        for x, y, _, _, _ in train_data:
-            batch_idx += 1
-            if batch_idx >= batches_per_epoch:
-                break
-
-            xc = (x[0].to(device), x[1]) # x[0] is a Tensor [batch_size, c, h, w], x[1] is a Tuple of `batch_size`` prompts
-            yc = [yy.to(device) for yy in y]
-            lossd = net.compute_loss(xc, yc)
-            loss = lossd['loss']
-
-
-            batch_n = 1
-            if batch_idx % batch_n == 0:
-                logging.info('Epoch: {}, Batch: {}, Loss: {:0.4f}'.format(epoch, batch_idx, loss.item()))
-
-            results['loss'] += loss.item()
-            for ln, l in lossd['losses'].items():
-                if ln not in results['losses']:
-                    results['losses'][ln] = 0
-                results['losses'][ln] += l.item()
-
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-            # Display the images
-            if vis:
-                imgs = []
-                n_img = min(4, x.shape[0])
-                for idx in range(n_img):
-                    imgs.extend([x[idx,].numpy().squeeze()] + [yi[idx,].numpy().squeeze() for yi in y] + [
-                        x[idx,].numpy().squeeze()] + [pc[idx,].detach().cpu().numpy().squeeze() for pc in
-                                                      lossd['pred'].values()])
-                gridshow('Display', imgs,
-                         [(xc.min().item(), xc.max().item()), (0.0, 1.0), (0.0, 1.0), (-1.0, 1.0),
-                          (0.0, 1.0)] * 2 * n_img,
-                         [cv2.COLORMAP_BONE] * 10 * n_img, 10)
-                cv2.waitKey(2)
-
-    results['loss'] /= batch_idx
-    for l in results['losses']:
-        results['losses'][l] /= batch_idx
-
-    return results
-
-
 def run():
     args = parse_args()
    
@@ -260,9 +133,6 @@ def run():
     console.setFormatter(formatter)
     # add the handler to the root logger
     logging.getLogger('').addHandler(console)
-
-    # Get the compute device
-    device = get_device(args.force_cpu)
 
     # Load Dataset
     logging.info('Loading {} Dataset...'.format(args.dataset.title()))
@@ -309,57 +179,14 @@ def run():
     logging.info('Loading network...')
     net = make_model(args)
 
-    if args.optim.lower() == 'adam':
-        optimizer = optim.Adam(net.parameters())
-    elif args.optim.lower() == 'sgd':
-        optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=0.9)
-    else:
-        raise NotImplementedError('Optimizer {} is not implemented'.format(args.optim))
-
-    # # Print model params    
-    # f = open(os.path.join(save_folder, 'arch.txt'), 'w')
-    # sys.stdout = f
-    # trainable_params_count = count_parameters(net)
-    # sys.stdout = sys.__stdout__
-    # f.close()
-    # logging.info('Trainable Parameters: {}'.format(trainable_params_count))
-
     # Set up Pytorch Lightning Module
-    model = LGraspModule(net, net.compute_loss, optimizer, None)
+    model = LGraspModule(net, net.compute_loss, dataset)
 
+    # Set up Pytorch Lightning Trainer
     trainer = make_trainer(args)
+
     # Fit model using train and val dataloader
     trainer.fit(model, train_data, val_data)
-
-
-    # best_iou = 0.0
-    # for epoch in range(args.epochs):
-    #     logging.info('Beginning Epoch {:02d}'.format(epoch))
-    #     train_results = train(epoch, net, device, train_data, optimizer, args.batches_per_epoch, vis=args.vis)
-
-    #     # Log training losses to tensorboard
-    #     tb.add_scalar('loss/train_loss', train_results['loss'], epoch)
-    #     for n, l in train_results['losses'].items():
-    #         tb.add_scalar('train_loss/' + n, l, epoch)
-
-    #     # Run Validation
-    #     logging.info('Validating...')
-    #     test_results = validate(net, device, val_data, args.iou_threshold)
-    #     logging.info('%d/%d = %f' % (test_results['correct'], test_results['correct'] + test_results['failed'],
-    #                                  test_results['correct'] / (test_results['correct'] + test_results['failed'])))
-
-    #     # Log validation results to tensorbaord
-    #     tb.add_scalar('loss/IOU', test_results['correct'] / (test_results['correct'] + test_results['failed']), epoch)
-    #     tb.add_scalar('loss/val_loss', test_results['loss'], epoch)
-    #     for n, l in test_results['losses'].items():
-    #         tb.add_scalar('val_loss/' + n, l, epoch)
-
-    #     # Save best performing network
-    #     iou = test_results['correct'] / (test_results['correct'] + test_results['failed'])
-    #     if iou > best_iou or epoch == 0 or (epoch % 10) == 0:
-    #         torch.save(net, os.path.join(save_folder, 'epoch_%02d_iou_%0.2f' % (epoch, iou)))
-    #         best_iou = iou
-
 
 if __name__ == '__main__':
     run()
