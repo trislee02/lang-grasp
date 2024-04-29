7d3e473514b73e569c992cf0558058a0d487f572
Le Quang Tri <quangtri2002lk@gmail.com>
2024-04-29 10:56:17 +0700

    Update post_process.py

--- inference/post_process.py
+++ inference/post_process.py
@@ -15,8 +15,8 @@ def post_process_output(q_img, cos_img, sin_img, width_img):
     ang_img = (torch.atan2(sin_img, cos_img) / 2.0).cpu().numpy().squeeze()
     width_img = width_img.cpu().numpy().squeeze() * 150.0
 
-    # q_img = gaussian(q_img, 2.0, preserve_range=True)
-    # ang_img = gaussian(ang_img, 2.0, preserve_range=True)
-    # width_img = gaussian(width_img, 1.0, preserve_range=True)
+    q_img = gaussian(q_img, 2.0, preserve_range=True)
+    ang_img = gaussian(ang_img, 2.0, preserve_range=True)
+    width_img = gaussian(width_img, 1.0, preserve_range=True)
 
     return q_img, ang_img, width_img
