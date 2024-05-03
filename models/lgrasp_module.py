77ee22be18f69d3988529313a2d9b4e20b56b263
Le Quang Tri <quangtri2002lk@gmail.com>
2024-05-03 21:50:44 +0700

    plot pred to wandb

--- models/lgrasp_module.py
+++ models/lgrasp_module.py
@@ -54,8 +54,11 @@ class LGraspModule(pl.LightningModule):
         self.log("train_loss", loss)
 
         plot_img = [wandb.Image(x[0][0], caption=x[1][0]), 
-                    wandb.Image(self.model.out_logits_per_image[0], caption=f"{x[1][0]}-logits")]       
-        plot_img.extend([wandb.Image(self.model.out_image_features[0][i], caption=f"{x[1][0]}-image features channel {i}") for i in range(10)])
+                    wandb.Image(lossd['pred']['pos'][0][0], caption=f"{x[1][0]}-pred_pos"),
+                    wandb.Image(lossd['pred']['cos'][0][0], caption=f"{x[1][0]}-pred_cos"),
+                    wandb.Image(lossd['pred']['sin'][0][0], caption=f"{x[1][0]}-pred_sin"),
+                    wandb.Image(lossd['pred']['width'][0][0], caption=f"{x[1][0]}-pred_width")]  
+             
         wandb_logger = self.logger.experiment
         wandb_logger.log({"plot": plot_img})
                                  
