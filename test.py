4d76974ab141a11fb6a48ce02c69117c0c544d75
Le Quang Tri <quangtri2002lk@gmail.com>
2024-05-03 20:40:09 +0700

    Update test.py

--- test.py
+++ test.py
@@ -17,7 +17,7 @@ transform = transforms.Compose(
 )
 image = transform(image).unsqueeze(0)
 image = image.cuda()
-text_features, image_features = module((image, ('grasp the mug at the handle')))
+text_features, image_features = module((image, ('grasp the mug at the handle', )))
 
 fig, ax = plt.subplots(nrows=1, ncols=1)
 img = image_features[0][0].detach().cpu().numpy()
